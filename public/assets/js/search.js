dec09d99d7b81f5a23846fdc3acbf21a8ded2e0e
Nathan Huber <Nathan_Huber@yahoo.com>
2020-02-11 02:56:31 -0600

    Added a drop-down box on the search page.  Added search.js file to assets/js folder.  Code for searching by title on search button click added.  It's not quite working yet.

---
+++ public/assets/js/search.js
@@ -0,0 +1,23 @@
+// wrap; make sure we wait to attach our handlers until the DOM is fully loaded.
+
+
+$("#searchBtn").on("click", function() {
+  var pCastSearch = $("#searchBox")
+  pCastSearch = pCastSearch.replace(/\s+/g, "").toLowerCase();
+  .val()
+  .trim();
+  $.get("/api/search/" + pCastSearch, function(data) {
+    console.log(data);
+
+    $("#searchResults").empty();
+
+    $("#searchResults").append("<h2" + data + "</h2>");
+
+  });
+});
+
+
+
+
+
+
