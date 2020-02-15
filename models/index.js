8b34f4cc3d894d9aa9ee71e52b6b4044028cabda
Andrelbarreto <andrebarreto77@gmail.com>
2020-02-15 13:09:16 -0600

    commiting user info

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ const db = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, require("../config/private.js"), config);
 }
 
 fs
